d7bbef94fb45fea94f29e62b93407530fe249585
Cosmin Sandu <cosmin.sandu@cloudtroopers.com>
2014-04-30 11:14:24 +0300

    merge reward points with memebr update DTO

--- src/StompMessagePublisher.java
+++ src/StompMessagePublisher.java
@@ -17,13 +17,16 @@ import org.apache.commons.cli.PosixParser;
 
 public class StompMessagePublisher {
 
-    public static final String REWARD_POINTS_JSON = "{\"loyaltyAccountId\":\"6761023031994\", \"externalProviderId\":\"4134361776535813\", \"points\":\"1\", \"sign\":\"+\", \"offerCode\":\"PR0141\", \"forfeitFlag\":\"N\", \"parsingBatch\":\"1391079208_636438973\", \"channelId\":1, \"transactionSourceId\":1, \"transactionComment\":\"coment\", \"transactionTime\":1389782282, \"transactionType\":\"E\", \"operatorId\":-1, \"token\":\"8eb0a18834d37b94e398e64677bcd85d\"}";
-    public static final String MEMBER_REGISTRATION_JSON = "{\"externalProviderId\":\"4134361776535813\",\"lastName\":\"Fallon\",\"firstName\":\"Cally\",\"middleInitial\":\" \",\"prefix\":\"AMiss\",\"suffix\":\"Ph.D\",\"fullName\":\"Fallon Cally\",\"addressLine1\":\"2165 Libero.Rd.\",\"addressLine2\":\"Ap #875-3531 Et Rd.\",\"city\":\"Jandrain-Jandrenouille\",\"state\":\"AK\",\"zipCode\":\".20083\",\"primaryPhoneNumber\":\"136567581\",\"secondaryPhoneNumber\":\"6125669026\",\"email\":\"0Cally.Fallon@email.com\",\"allegiantCustomerId\":\"111111\",\"parsingBatch\":\"1390473784_298389220\",\"channelId\":1,\"token\":\"myToken\"}";
-    public static final String MEMBER_UPDATE_JSON = "{\"loyaltyAccountId\":\"6761023031994\",\"externalProviderId\":\"4134361776535813\",\"fullName\":\"Mr Clayton Andrei Jr\",\"addressLine1\":\"4590 Wilkinson Street\",\"addressLine2\":\"6009 ON AAAAAAAAA\",\"city\":\"Austin\",\"state\":\"CA\",\"zipCode\":\"6186\",\"email\":\"AndreiClayton@rhyta.com\",\"parsingBatch\":\"1397817086_603333107\", \"token\":\"f971c633287139e384f950f58067e8c7\"}";
+    private static final long FBC_ACCT_ID = 7891956190123332L;
+
+    public static final String REWARD_POINTS_JSON_MERGE = "{\"recordId\":\"11123\", \"loyaltyAccountId\":\"6761023031994\", \"externalProviderId\":\"4134361776535813\",\"fullName\":\"Mr Clayton Andrei Jr\",\"addressLine1\":\"4590 Wilkinson Street\",\"addressLine2\":\"6009 ON AAAAAAAAA\",\"city\":\"Austin\",\"state\":\"CA\",\"zipCode\":\"6186\",\"email\":\"AndreiClayton@rhyta.com\",\"points\":\"1\", \"sign\":\"+\", \"offerCode\":\"PR0141\", \"forfeitFlag\":\"N\", \"parsingBatch\":\"1391079208_636438973\", \"channelId\":1, \"transactionSourceId\":1, \"transactionComment\":\"coment\", \"transactionTime\":1389782282, \"transactionType\":\"E\", \"operatorId\":-1, \"token\":\"8eb0a18834d37b94e398e64677bcd85d\"}";
+   // public static final String REWARD_POINTS_JSON = "{\"loyaltyAccountId\":\"6761023031994\", \"externalProviderId\":\"4134361776535813\", \"points\":\"1\", \"sign\":\"+\", \"offerCode\":\"PR0141\", \"forfeitFlag\":\"N\", \"parsingBatch\":\"1391079208_636438973\", \"channelId\":1, \"transactionSourceId\":1, \"transactionComment\":\"coment\", \"transactionTime\":1389782282, \"transactionType\":\"E\", \"operatorId\":-1, \"token\":\"8eb0a18834d37b94e398e64677bcd85d\"}";
+    public static final String MEMBER_REGISTRATION_JSON = "{\"externalProviderId\":\""+FBC_ACCT_ID+"\",\"lastName\":\"Fallon\",\"firstName\":\"Cally\",\"middleInitial\":\" \",\"prefix\":\"AMiss\",\"suffix\":\"Ph.D\",\"fullName\":\"Fallon Cally\",\"addressLine1\":\"2165 Libero.Rd.\",\"addressLine2\":\"Ap #875-3531 Et Rd.\",\"city\":\"Jandrain-Jandrenouille\",\"state\":\"AK\",\"zipCode\":\".20083\",\"primaryPhoneNumber\":\"136567581\",\"secondaryPhoneNumber\":\"6125669026\",\"email\":\"0Cally.Fallon@email.com\",\"allegiantCustomerId\":\"111111\",\"parsingBatch\":\"1390473784_298389220\",\"channelId\":1,\"token\":\"myToken\"}";
+   // public static final String MEMBER_UPDATE_JSON = "{\"loyaltyAccountId\":\"6761023031994\",\"externalProviderId\":\"4134361776535813\",\"fullName\":\"Mr Clayton Andrei Jr\",\"addressLine1\":\"4590 Wilkinson Street\",\"addressLine2\":\"6009 ON AAAAAAAAA\",\"city\":\"Austin\",\"state\":\"CA\",\"zipCode\":\"6186\",\"email\":\"AndreiClayton@rhyta.com\",\"parsingBatch\":\"1397817086_603333107\", \"token\":\"f971c633287139e384f950f58067e8c7\"}";
 
     public static final String REWARD_POINTS_JMS_DESTINATION = "jms.queue.loyalty.exported.rewardPoints";
     public static final String MEMBER_REGISTRATION_JMS_DESTINATION = "jms.queue.loyalty.exported.memberRegistration";
-    public static final String MEMBER_UPDATE_JMS_DESTINATION = "jms.queue.loyalty.exported.memberUpdate";
+  //  public static final String MEMBER_UPDATE_JMS_DESTINATION = "jms.queue.loyalty.exported.memberUpdate";
 
     public static void main(String[] args) throws ParseException {
 
@@ -38,12 +41,22 @@ public class StompMessagePublisher {
         CommandLineParser parser = new PosixParser();
         CommandLine cmd = parser.parse(options, args);
 
-        String host = "127.0.0.1";
+        //String host = "loyjbsms11-public.lolacloud.com";
+       // String host = "pink.cloudtroopers.ro";
+        String host = "localhost";
+        // String host = "pink.cloudtroopers.ro";
         String port = "61613";
         String user = "guest";
         String pass = "P@ssword1";
-        String destination = MEMBER_UPDATE_JMS_DESTINATION;
-        String json = MEMBER_UPDATE_JSON;
+
+//        String destination = MEMBER_REGISTRATION_JMS_DESTINATION;
+//        String json = MEMBER_REGISTRATION_JSON;
+
+//        String destination = MEMBER_UPDATE_JMS_DESTINATION;
+//        String json = MEMBER_UPDATE_JSON;
+
+        String destination = REWARD_POINTS_JMS_DESTINATION;
+        String json = REWARD_POINTS_JSON_MERGE;
 
         if (cmd.hasOption("h")) {
             host = cmd.getOptionValue("h");
@@ -71,7 +84,9 @@ public class StompMessagePublisher {
             connection.connect(user, pass);
 
             for (int i = 0; i < 1; i++) {
-                send(host, port, user, pass, destination, json, connection);
+                System.out.println(" msg " + i);
+                String newJson = json.replaceAll(""+FBC_ACCT_ID,""+(FBC_ACCT_ID+i));
+                send(host, port, user, pass, destination, newJson, connection);
             }
             connection.disconnect();
             connection.close();
