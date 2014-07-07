306ae318811c6e6635dd36c6ae1b3512fa3e7ace
Roland.Kozma <roland.kozma@stash.allegiantair.com>
2014-07-07 16:06:22 +0300

    updated

--- src/StompMessagePublisher.java
+++ src/StompMessagePublisher.java
@@ -19,14 +19,13 @@ public class StompMessagePublisher {
 
     private static final long FBC_ACCT_ID = 7891956190123332L;
 
-    public static final String REWARD_POINTS_JSON_MERGE = "{\"recordId\":\"11123\", \"loyaltyAccountId\":\"6761023031994\", \"externalProviderId\":\"4134361776535813\",\"fullName\":\"Mr Clayton Andrei Jr\",\"addressLine1\":\"4590 Wilkinson Street\",\"addressLine2\":\"6009 ON AAAAAAAAA\",\"city\":\"Austin\",\"state\":\"CA\",\"zipCode\":\"6186\",\"email\":\"AndreiClayton@rhyta.com\",\"points\":\"1\", \"sign\":\"+\", \"offerCode\":\"PR0141\", \"forfeitFlag\":\"N\", \"parsingBatch\":\"1391079208_636438973\", \"channelId\":1, \"transactionSourceId\":1, \"transactionComment\":\"coment\", \"transactionTime\":1389782282, \"transactionType\":\"E\", \"operatorId\":-1, \"token\":\"8eb0a18834d37b94e398e64677bcd85d\"}";
-   // public static final String REWARD_POINTS_JSON = "{\"loyaltyAccountId\":\"6761023031994\", \"externalProviderId\":\"4134361776535813\", \"points\":\"1\", \"sign\":\"+\", \"offerCode\":\"PR0141\", \"forfeitFlag\":\"N\", \"parsingBatch\":\"1391079208_636438973\", \"channelId\":1, \"transactionSourceId\":1, \"transactionComment\":\"coment\", \"transactionTime\":1389782282, \"transactionType\":\"E\", \"operatorId\":-1, \"token\":\"8eb0a18834d37b94e398e64677bcd85d\"}";
-    public static final String MEMBER_REGISTRATION_JSON = "{\"recordId\":\"10123\", \"externalProviderId\":\""+FBC_ACCT_ID+"\",\"lastName\":\"Fallon\",\"firstName\":\"Cally\",\"middleInitial\":\" \",\"prefix\":\"AMiss\",\"suffix\":\"Ph.D\",\"fullName\":\"Fallon Cally\",\"addressLine1\":\"2165 Libero.Rd.\",\"addressLine2\":\"Ap #875-3531 Et Rd.\",\"city\":\"Jandrain-Jandrenouille\",\"state\":\"AK\",\"zipCode\":\".20083\",\"primaryPhoneNumber\":\"136567581\",\"secondaryPhoneNumber\":\"6125669026\",\"email\":\"0Cally.Fallon@email.com\",\"allegiantCustomerId\":\"111111\",\"parsingBatch\":\"1390473784_298389220\",\"channelId\":1,\"token\":\"myToken\"}";
-   // public static final String MEMBER_UPDATE_JSON = "{\"loyaltyAccountId\":\"6761023031994\",\"externalProviderId\":\"4134361776535813\",\"fullName\":\"Mr Clayton Andrei Jr\",\"addressLine1\":\"4590 Wilkinson Street\",\"addressLine2\":\"6009 ON AAAAAAAAA\",\"city\":\"Austin\",\"state\":\"CA\",\"zipCode\":\"6186\",\"email\":\"AndreiClayton@rhyta.com\",\"parsingBatch\":\"1397817086_603333107\", \"token\":\"f971c633287139e384f950f58067e8c7\"}";
+    public static final String REWARD_POINTS_JSON_MERGE = "{\"recordId\":\"11123\", \"loyaltyAccountId\":\"6761023031987\", \"externalProviderId\":\"7891956190123332\",\"fullName\":\"Mr Clayton Andrei Jr\",\"addressLine1\":\"4590 Wilkinson Street\",\"addressLine2\":\"6009 ON AAAAAAAAA\",\"city\":\"Austin\",\"state\":\"CA\",\"zipCode\":\"6186\",\"email\":\"AndreiClayton@rhyta.com\",\"points\":\"100\", \"sign\":\"+\", \"offerCode\":\"PR0141\", \"forfeitFlag\":\"N\", \"parsingBatch\":\"1391079208_636438973\", \"channelName\":\"FNBO Premium Card\", \"transactionComment\":\"coment\", \"transactionTime\":1389782282, \"transactionType\":\"E\", \"operatorId\":-1, \"token\":\"8eb0a18834d37b94e398e64677bcd85d\"}";
+    public static final String MEMBER_REGISTRATION_JSON = "{\"recordId\":\"10123\", \"externalProviderId\":\""
+            + FBC_ACCT_ID
+            + "\",\"lastName\":\"Fallon\",\"firstName\":\"Cally\",\"middleInitial\":\" \",\"prefix\":\"AMiss\",\"suffix\":\"Ph.D\",\"fullName\":\"Fallon Cally\",\"addressLine1\":\"2165 Libero.Rd.\",\"addressLine2\":\"Ap #875-3531 Et Rd.\",\"city\":\"Jandrain-Jandrenouille\",\"state\":\"AK\",\"zipCode\":\".20083\",\"primaryPhoneNumber\":\"136567581\",\"secondaryPhoneNumber\":\"6125669026\",\"email\":\"0Cally.Fallon@email.com\",\"allegiantCustomerId\":\"111111\",\"parsingBatch\":\"1390473784_298389220\",\"channelName\":\"FNBO Premium Card\",\"token\":\"myToken\"}";
 
     public static final String REWARD_POINTS_JMS_DESTINATION = "jms.queue.loyalty.exported.rewardPoints";
     public static final String MEMBER_REGISTRATION_JMS_DESTINATION = "jms.queue.loyalty.exported.memberRegistration";
-  //  public static final String MEMBER_UPDATE_JMS_DESTINATION = "jms.queue.loyalty.exported.memberUpdate";
 
     public static void main(String[] args) throws ParseException {
 
@@ -41,22 +40,19 @@ public class StompMessagePublisher {
         CommandLineParser parser = new PosixParser();
         CommandLine cmd = parser.parse(options, args);
 
-        //String host = "loyjbsms11-public.lolacloud.com";
-       // String host = "pink.cloudtroopers.ro";
+        // String host = "loyjbsms11-public.lolacloud.com";
+        // String host = "pink.cloudtroopers.ro";
         String host = "localhost";
         // String host = "pink.cloudtroopers.ro";
         String port = "61613";
         String user = "guest";
         String pass = "P@ssword1";
 
-//        String destination = MEMBER_REGISTRATION_JMS_DESTINATION;
-//        String json = MEMBER_REGISTRATION_JSON;
-
-//        String destination = MEMBER_UPDATE_JMS_DESTINATION;
-//        String json = MEMBER_UPDATE_JSON;
-
         String destination = REWARD_POINTS_JMS_DESTINATION;
         String json = REWARD_POINTS_JSON_MERGE;
+
+        // String destination = MEMBER_REGISTRATION_JMS_DESTINATION;
+        // String json = MEMBER_REGISTRATION_JSON;
 
         if (cmd.hasOption("h")) {
             host = cmd.getOptionValue("h");
@@ -85,7 +81,7 @@ public class StompMessagePublisher {
 
             for (int i = 0; i < 1; i++) {
                 System.out.println(" msg " + i);
-                String newJson = json.replaceAll(""+FBC_ACCT_ID,""+(FBC_ACCT_ID+i));
+                String newJson = json.replaceAll("" + FBC_ACCT_ID, "" + (FBC_ACCT_ID + i));
                 send(host, port, user, pass, destination, newJson, connection);
             }
             connection.disconnect();
